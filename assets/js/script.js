da2971c209683c1c47a5daac8c54552a9b9e992c
Idzel Jaimes <irjaimes@outlook.com>
2020-12-22 22:18:04 -0800

    added eventlistener for form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 //DOM element variables
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //TaskHandler Function
-var createTaskHandler = function () {
+var createTaskHandler = function (event) { //pass event so that page doesn't refresh
+    event.preventDefault(); //stops page from refreshing on submit
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+    console.log(event);
+
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
