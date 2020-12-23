00d13f0cfa63a6e609e097cbbc89d99fd9d5c149
Idzel Jaimes <irjaimes@outlook.com>
2020-12-22 23:03:24 -0800

    added capture form fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,27 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //TaskHandler Function
 var createTaskHandler = function (event) { //pass event so that page doesn't refresh
     event.preventDefault(); //stops page from refreshing on submit
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-    console.log(event);
 
-}
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give new div class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to new div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl)
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl)
+};
 
 formEl.addEventListener("submit", createTaskHandler);
 
